0a3cc7c90c420c45e3e65e6392ffe663dc09ceee
grid23 <hello@grid23.net>
2018-12-19 09:25:36 +0100

    cleanswipe

--- specs/client/routes/index.mjs
+++
@@ -1,9 +0,0 @@
-"use strict"
-
-import esmodule from "./esmodule.mjs"
-import external from "./external.mjs"
-import root from "./root.mjs"
-import specs from "./specs.mjs"
-import samples from "./samples.mjs"
-
-export default [ esmodule, external, root, specs, samples ]
